4e8998ab39c12a394ea59781caed95658d7e2007
aa.adamki <aa.adamki@centiro.se>
2024-09-04 21:13:58 +0200

    init commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { YoutubeComponent } from './youtube/youtube.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SignupComponent } from './signup/signup.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'youtube', component: YoutubeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'contact', component: ContactComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
